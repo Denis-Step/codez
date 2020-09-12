c0b573827854be0901f885b36695ee7447fab383
DENIS <cheerios4cat@gmail.com>
2020-09-12 17:24:45 -0400

    Commiting old copy

--- game.py
+++ game.py
@@ -6,16 +6,16 @@ import random
 from diego import Word, User, GameHistory
 
 class Game:
-    engine = create_engine('sqlite:///db.db', echo=True)
-    Base = declarative_base()
-    Session = sessionmaker(bind=engine)
-    Session.configure(bind=engine)
-    session = Session()
 
     def __init__(self):
-        self.words = self.createWordDict()
+        #Words: Red, Blue, Black, Bomb, + revealed at end
+        self.words = self.create_word_dict()
+        self.turn = "Red"
+        self.redPoints = 0
+        self.bluePoints = 0
+        self.winner = None
 
-    def createWordDict(self):
+    def create_word_dict(self):
         engine = create_engine('sqlite:///db.db', echo=True)
         Base = declarative_base()
         Session = sessionmaker(bind=engine)
@@ -31,6 +31,7 @@ class Game:
 
             word = session.query(Word).filter(Word.wordid == j)
             word = word.value("word")
+            print(f'{word} and ID {j}')
 
             if word not in randdict:
                 if Red < 9:
@@ -54,3 +55,25 @@ class Game:
         session.close()
         return shuffleddict
         print(shuffleddict)
+
+    def reveal_word(self,word):
+        #Check for Bomb first:
+        if self.words[word] == "Bomb":
+            self.set_winner('Red') if self.turn == "Blue" else self.set_winner("Blue")
+        elif self.words[word] == "Red":
+            self.words[word] = self.words[word] + "Revealed"
+            self.redPoints += 1 if self.turn == "Red" else "Blue" 
+        elif self.words[word] == "Blue":
+            self.words[word] = self.words[word] + "Revealed"
+            self.bluePoints += 1 if self.turn == "Blue" else "Blue"
+        elif self.words[word] == "Black":
+            self.words[word] = self.words[word] + "Revealed"
+            if self.turn == "Red":
+                self.turn = "Blue"
+            else:
+                self.turn = "Red"
+
+    
+    def set_winner(self,team):
+        #TODO
+        pass
--- wordLoad.py
+++
@@ -1,7 +0,0 @@
-from nltk.corpus import wordnet as wn
-import diego_query as db
-
-with open('5lenwords.txt') as f:
-    for line in f:
-        word = line.strip()
-        db.create_word(word)
