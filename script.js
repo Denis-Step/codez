0e4a6e7c32b39eb7cf90cecbd242aab54adf05b4
DENIS <cheerios4cat@gmail.com>
2020-09-12 02:50:52 -0400

    Done with front-end for now

--- script.js
+++ script.js
@@ -162,7 +162,7 @@
 /*!*********************!*\
   !*** ./src/Cell.js ***!
   \*********************/
-/*! exports provided: default */function(module,__webpack_exports__,__webpack_require__){"use strict";eval('__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Cell; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Cell(props) {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n    className: "cell red"\n  }, props.word);\n}\n\n//# sourceURL=webpack:///./src/Cell.js?')},"./src/Game.js":
+/*! exports provided: default */function(module,__webpack_exports__,__webpack_require__){"use strict";eval('__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Cell; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Cell(props) {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n    className: "cell"\n  }, props.word);\n}\n\n//# sourceURL=webpack:///./src/Cell.js?')},"./src/Game.js":
 /*!*********************!*\
   !*** ./src/Game.js ***!
   \*********************/
