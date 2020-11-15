07a379f6059aceb9de64da8a812963f955e9141b
Denis Step <cheerios4cat@gmail.com>
2020-11-15 06:46:05 +0000

    More work on redis

--- services.py
+++ services.py
@@ -12,17 +12,34 @@ engine = create_engine('sqlite:///db.db', echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 Session.configure(bind=engine)
-
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 NUM_WORDS = 5757
 
 # TODO: Make The Red/Blue/Neutral/Bomb Constants
 def get_state(game_ID):
-    words = r.hgetall(game_ID)
-    return words
+    '''Return player and word state'''
+    state = {'playerState': r.hgetall('state:' + game_ID),
+        'wordsState': r.hgetall('words:' + game_ID)}
+    return state
 
-def create_game(game_ID, db=0):
+def create_game(game_ID):
+    '''Takes a UUID and creates two hashes, one for the player state
+    and a second for the words. Throws an exception if either table
+    cannot be created. Returns the dict of words. '''
+    
+    new_game = {'turn': 'blue-spymaster',
+        'hint': '',
+        'attemptsLeft': 0,
+        'redPoints': 0,
+        'bluePoints': 0}
+    words = create_board()
+    set_fields = r.hset('state:' + game_ID, mapping=new_game)
+    set_fields += r.hset('words:' + game_ID, mapping=words)
+    return {'playerState': new_game, 'wordsState': words} if set_fields == 30 else{}
+
+# TODO: Refactor this!!
+def create_board():
     session = Session()
     words = dict()
 
@@ -36,32 +53,30 @@ def create_game(game_ID, db=0):
             continue
 
         if Red < 9:
-            words[word] = "Red"
+            words[word] = "red"
             Red += 1
             i += 1
         elif Blue < 8:
-            words[word] = "Blue"
+            words[word] = "blue"
             Blue += 1
             i += 1
         elif Neutral < 7:
-            words[word] = "Neutral"
+            words[word] = "neutral"
             Neutral += 1
             i += 1
         elif Bomb < 1:
-            words[word] = "Bomb"
+            words[word] = "bomb"
             Bomb += 1
             i += 1
-
+    
+    session.close()
     keys = list(words.keys())
     random.shuffle(keys)
     shuffledDict = dict()
     for key in keys:
         shuffledDict.update({key: words[key]})
-    session.close()
 
-    r.hmset(game_ID, words)
     return words
-
 
 def lch(word_one, word_two):
     word_one = wn.synsets(word_one)[0]
