770200aefc18b8c4f3e5e15c0d99511008139ac0
Denis Stepanenko <cheerios4cat@gmail.com>
2021-02-13 21:20:15 -0500

    Login working

--- app/models/test_models.py
+++ app/models/test_models.py
@@ -3,5 +3,10 @@ from models.models import Word, User
 
 
 def test_create_user(db):
-    user = User.create("test", "dummy")
+    user = User.create("test", "password")
     assert user.name == "test"
+    assert User.exists("test")
+
+
+def test_login_user(db):
+    assert User.login("test", "password")
