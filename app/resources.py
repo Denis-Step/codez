f56d3c9fe4cd72a40ae75390a9c562608376b56c
Denis Stepanenko <cheerios4cat@gmail.com>
2021-03-21 23:08:05 -0400

    hypernym resource working

--- app/resources.py
+++ app/resources.py
@@ -55,9 +55,11 @@ class DefinitionResource(Resource):
 
 class HypernymResource(Resource):
     def get(self):
-        words = request.args.get("words", None)
-        try:
-            hyps = models.Word.common_hypernyms(words[0], words[1])
-            return jsonify(hyps)
-        except Exception:
-            return (None, 404)
+        words = request.args.get("words", None).split(" ")
+        print(words)
+        if not words:
+            return jsonify([])
+
+        hyps = models.Word.common_hypernyms(words[0], words[1])
+        print(hyps)
+        return jsonify(list(hyps))
